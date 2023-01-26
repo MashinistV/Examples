3bf67a94443b4a8608a0a2244b0cc2b988db892f
Sergey <mashinistv@gmail.com>
2023-01-26 22:35:24 +0300

    006 Example

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 2;
+int b = 4;
+int c = 0;
+int d = 5;
+int e = 3;
+
+int max = a;
+
+if (max < b) max = b;
+if (max < c) max = c;
+if (max < d) max = d;
+if (max < e) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
